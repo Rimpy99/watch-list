c5573ca389e276e1c0575ef9fa3ba1099ca8b4f4
pawelwrona <pawelwrona@air-pawelwrona.home>
2023-08-12 10:16:24 +0200

    [CLIENT] saving data from response to movies state added

--- client/src/components/MoviesList.tsx
+++ client/src/components/MoviesList.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { JsxEmit } from "typescript";
 
 type MoviesListPropsType = {
     currentPage: number,
@@ -7,34 +8,56 @@ type MoviesListPropsType = {
 type MovieType = {
     id: number,
     title: string,
+    overview: string,
     posterPath: string,
+}
+
+type ResponseMovieType = {
+    id: number,
+    title: string,
+    overview: string,
+    poster_path: string,
 }
 
 const MoviesList = ({currentPage}: MoviesListPropsType) => {
 
-    const [ movies, setMovies ] = useState<MovieType[] | null>(null);
+    const [ movies, setMovies ] = useState<MovieType[] | []>([]);
 
     //COPIED FROM TMDB DOCUMENTATION
     const fetchMovies = () => {
+
         const options = {
             method: 'GET',
             headers: {
               accept: 'application/json',
-              Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`
+              Authorization: `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`
             }
           };
           
           fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${currentPage}`, options)
             .then(response => response.json())
-            .then(response => console.log(response))
+            .then(response => {
+                response.results.map((movie: ResponseMovieType)=> {
+                    const movieData = {
+                        id: movie.id,
+                        title: movie.title,
+                        overview: movie.overview,
+                        posterPath: movie.poster_path,
+                    }
+
+                    setMovies(currentArray => [...currentArray, movieData]);
+                    // console.log(movieData)
+                })
+            })
             .catch(err => console.error(err));
     }
 
     useEffect(() => {
+        setMovies([]);
         fetchMovies();
-    }, []);
+    }, [currentPage]);
 
-    if(!movies){
+    if(!movies.length){
         return (
             <div className="min-h-[80vh]">
                 <h3 className="text-center py-10">Couldn't get any data!</h3>
@@ -44,6 +67,14 @@ const MoviesList = ({currentPage}: MoviesListPropsType) => {
 
     return(
         <>
+            <h1>{currentPage}</h1>
+            {
+                movies.map((movie) => {
+                    return(
+                        <h1>{movie.title}</h1>
+                    )
+                })
+            }
         </>
     )
 }
