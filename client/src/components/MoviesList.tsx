903e8c2c930ea15fd86d4c5e7e4f41b59030b263
pawelwrona <pawelwrona@air-pawelwrona.home>
2023-08-12 11:12:54 +0200

    [CLIENT] Error handling and displaying loading component when there is no data in a state added

--- client/src/components/MoviesList.tsx
+++ client/src/components/MoviesList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { JsxEmit } from "typescript";
+import BarLoader from "react-spinners/BarLoader";
+import { flexCenter } from "../styles/styles";
 
 type MoviesListPropsType = {
     currentPage: number,
@@ -22,6 +23,7 @@ type ResponseMovieType = {
 const MoviesList = ({currentPage}: MoviesListPropsType) => {
 
     const [ movies, setMovies ] = useState<MovieType[] | []>([]);
+    const [ isError, setIsError ] = useState<boolean>(false);
 
     //COPIED FROM TMDB DOCUMENTATION
     const fetchMovies = () => {
@@ -34,40 +36,50 @@ const MoviesList = ({currentPage}: MoviesListPropsType) => {
             }
           };
           
-          fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${currentPage}`, options)
-            .then(response => response.json())
-            .then(response => {
-                response.results.map((movie: ResponseMovieType)=> {
-                    const movieData = {
-                        id: movie.id,
-                        title: movie.title,
-                        overview: movie.overview,
-                        posterPath: movie.poster_path,
-                    }
-
-                    setMovies(currentArray => [...currentArray, movieData]);
-                    // console.log(movieData)
-                })
+        fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${currentPage}`, options)
+        .then(response => response.json())
+        .then(response => {
+            response.results.map((movie: ResponseMovieType)=> {
+                const movieData = {
+                    title: movie.title,
+                    id: movie.id,
+                    posterPath: movie.poster_path,
+                    overview: movie.overview,    
+                }
+                setMovies(currentArray => [...currentArray, movieData]);
+                setIsError(false)
             })
-            .catch(err => console.error(err));
+        })
+        .catch(err => {
+            console.error(err)
+            setIsError(true)
+        });
     }
 
     useEffect(() => {
         setMovies([]);
+        setIsError(false);
         fetchMovies();
     }, [currentPage]);
 
     if(!movies.length){
-        return (
-            <div className="min-h-[80vh]">
-                <h3 className="text-center py-10">Couldn't get any data!</h3>
+        if(isError) {
+            return (
+                <div className="min-h-[80vh]">
+                    <h3 className="text-center py-10">Couldn't get any data!</h3>
+                </div>
+            )
+        }
+
+        return(
+            <div className={`min-h-[80vh] ${flexCenter}`}>
+                <BarLoader color={'#e0e0e0'}/>
             </div>
         )
     }
 
     return(
         <>
-            <h1>{currentPage}</h1>
             {
                 movies.map((movie) => {
                     return(
